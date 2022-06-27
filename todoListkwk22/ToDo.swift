2f527be7a13ae6df8ba908dca0c5ee419a4eb3e6
Falak Grover <falakgrover09@gmail.com>
2022-06-27 16:47:53 -0500

    Finished Iteration 0

---
+++ todoListkwk22/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  todoListkwk22
+//
+//  Created by Scholar on 6/27/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = "SHOULDNT SEE THIS!!"
+    var important = false
+}
